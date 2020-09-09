6a17726021d72f4c0ae0dbd1455c3bac17ae41ba
fj <18813000271@163.com>
2020-09-09 15:05:52 +0800

    feat: 1.初始化sumu-auth

---
+++ SuMu-Cloud/sumu-auth/src/main/java/com/sumu/auth/SuMuAuthApplication.java
@@ -0,0 +1,30 @@
+package com.sumu.auth;
+
+import com.sumu.common.security.annotation.EnableRyFeignClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * 认证授权中心
+ *
+ * @author ruoyi
+ */
+@EnableRyFeignClients
+@SpringCloudApplication
+public class SuMuAuthApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SuMuAuthApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  认证授权中心启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
---
+++ SuMu-Cloud/sumu-auth/src/main/java/com/sumu/auth/controller/TokenController.java
@@ -0,0 +1,66 @@
+package com.sumu.auth.controller;
+
+import com.sumu.auth.form.LoginBody;
+import com.sumu.auth.service.SysLoginService;
+import com.sumu.common.core.domain.R;
+import com.sumu.common.core.utils.StringUtils;
+import com.sumu.common.security.service.TokenService;
+import com.sumu.system.api.model.LoginUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * token 控制
+ *
+ * @author ruoyi
+ */
+@RestController
+public class TokenController
+{
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private SysLoginService sysLoginService;
+
+    @PostMapping("login")
+    public R<?> login(@RequestBody LoginBody form)
+    {
+        // 用户登录
+        LoginUser userInfo = sysLoginService.login(form.getUsername(), form.getPassword());
+        // 获取登录token
+        return R.ok(tokenService.createToken(userInfo));
+    }
+
+    @DeleteMapping("logout")
+    public R<?> logout(HttpServletRequest request)
+    {
+        LoginUser loginUser = tokenService.getLoginUser(request);
+        if (StringUtils.isNotNull(loginUser))
+        {
+            String username = loginUser.getUsername();
+            // 删除用户缓存记录
+            tokenService.delLoginUser(loginUser.getToken());
+            // 记录用户退出日志
+            sysLoginService.logout(username);
+        }
+        return R.ok();
+    }
+
+    @PostMapping("refresh")
+    public R<?> refresh(HttpServletRequest request)
+    {
+        LoginUser loginUser = tokenService.getLoginUser(request);
+        if (StringUtils.isNotNull(loginUser))
+        {
+            // 刷新令牌有效期
+            return R.ok(tokenService.refreshToken(loginUser));
+        }
+        return R.ok();
+    }
+}
