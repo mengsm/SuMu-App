b3cb1ac8275c08bc2f84d1edc32af2bf72e5c88c
fengjian <fengj@whalekun.com>
2021-01-23 23:28:28 +0800

    初始化依赖

--- SuMu-Cloud/sumu-common/sumu-common-swagger/src/main/java/com/sumu/common/swagger/config/SwaggerAutoConfiguration.java
+++ SuMu-Cloud/sumu-common/sumu-common-swagger/src/main/java/com/sumu/common/swagger/config/SwaggerAutoConfiguration.java
@@ -1,7 +1,21 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
 package com.sumu.common.swagger.config;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -10,117 +24,118 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
+/**
+ * @author lengleng swagger配置 禁用方法1：使用注解@Profile({"dev","test"})
+ * 表示在开发或测试环境开启，而在生产关闭。（推荐使用） 禁用方法2：使用注解@ConditionalOnProperty(name = "swagger.enable",
+ * havingValue = "true") 然后在测试配置或者开发配置中添加swagger.enable=true即可开启，生产环境不填则默认关闭Swagger.
+ */
 @Configuration
-@EnableSwagger2
 @EnableAutoConfiguration
 @ConditionalOnProperty(name = "swagger.enabled", matchIfMissing = true)
-public class SwaggerAutoConfiguration
-{
-    /**
-     * 默认的排除路径，排除Spring Boot默认的错误处理路径和端点
-     */
-    private static final List<String> DEFAULT_EXCLUDE_PATH = Arrays.asList("/error", "/actuator/**");
+public class SwaggerAutoConfiguration {
 
-    private static final String BASE_PATH = "/**";
+	/**
+	 * 默认的排除路径，排除Spring Boot默认的错误处理路径和端点
+	 */
+	private static final List<String> DEFAULT_EXCLUDE_PATH = Arrays.asList("/error", "/actuator/**");
 
-    @Bean
-    @ConditionalOnMissingBean
-    public SwaggerProperties swaggerProperties()
-    {
-        return new SwaggerProperties();
-    }
+	private static final String BASE_PATH = "/**";
 
-    @Bean
-    public Docket api(SwaggerProperties swaggerProperties)
-    {
-        // base-path处理
-        if (swaggerProperties.getBasePath().isEmpty())
-        {
-            swaggerProperties.getBasePath().add(BASE_PATH);
-        }
-        // noinspection unchecked
-        List<Predicate<String>> basePath = new ArrayList<Predicate<String>>();
-        swaggerProperties.getBasePath().forEach(path -> basePath.add(PathSelectors.ant(path)));
+	@Bean
+	@ConditionalOnMissingBean
+	public SwaggerProperties swaggerProperties() {
+		return new SwaggerProperties();
+	}
 
-        // exclude-path处理
-        if (swaggerProperties.getExcludePath().isEmpty())
-        {
-            swaggerProperties.getExcludePath().addAll(DEFAULT_EXCLUDE_PATH);
-        }
-        List<Predicate<String>> excludePath = new ArrayList<>();
-        swaggerProperties.getExcludePath().forEach(path -> excludePath.add(PathSelectors.ant(path)));
+	@Bean
+	public Docket api(SwaggerProperties swaggerProperties) {
+		// base-path处理
+		if (swaggerProperties.getBasePath().isEmpty()) {
+			swaggerProperties.getBasePath().add(BASE_PATH);
+		}
 
-        //noinspection Guava
-        return new Docket(DocumentationType.SWAGGER_2)
-                .host(swaggerProperties.getHost())
-                .apiInfo(apiInfo(swaggerProperties)).select()
-                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
-                .paths(Predicates.and(Predicates.not(Predicates.or(excludePath)), Predicates.or(basePath)))
-                .build()
-                .securitySchemes(securitySchemes())
-                .securityContexts(securityContexts())
-                .pathMapping("/");
-    }
+		// exclude-path处理
+		if (swaggerProperties.getExcludePath().isEmpty()) {
+			swaggerProperties.getExcludePath().addAll(DEFAULT_EXCLUDE_PATH);
+		}
+		List<Predicate<String>> excludePath = new ArrayList<>();
+		swaggerProperties.getExcludePath().forEach(path -> excludePath.add(PathSelectors.ant(path)));
 
-    /**
-     * 安全模式，这里指定token通过Authorization头请求头传递
-     */
-    private List<ApiKey> securitySchemes()
-    {
-        List<ApiKey> apiKeyList = new ArrayList<ApiKey>();
-        apiKeyList.add(new ApiKey("Authorization", "Authorization", "header"));
-        return apiKeyList;
-    }
+		// 版本请求头处理
+		List<RequestParameter> pars = new ArrayList<>();
 
-    /**
-     * 安全上下文
-     */
-    private List<SecurityContext> securityContexts()
-    {
-        List<SecurityContext> securityContexts = new ArrayList<>();
-        securityContexts.add(
-                SecurityContext.builder()
-                        .securityReferences(defaultAuth())
-                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
-                        .build());
-        return securityContexts;
-    }
+		RequestParameterBuilder versionPar = new RequestParameterBuilder().description("灰度路由版本信息")
+				.in(ParameterType.HEADER).name("VERSION").required(false)
+				.query(param -> param.model(model -> model.scalarModel(ScalarType.STRING)));
 
-    /**
-     * 默认的全局鉴权策略
-     *
-     * @return
-     */
-    private List<SecurityReference> defaultAuth()
-    {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        List<SecurityReference> securityReferences = new ArrayList<>();
-        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
-        return securityReferences;
-    }
+		pars.add(versionPar.build());
 
-    private ApiInfo apiInfo(SwaggerProperties swaggerProperties)
-    {
-         return new ApiInfoBuilder()
-             .title(swaggerProperties.getTitle())
-             .description(swaggerProperties.getDescription())
-             .license(swaggerProperties.getLicense())
-             .licenseUrl(swaggerProperties.getLicenseUrl())
-             .termsOfServiceUrl(swaggerProperties.getTermsOfServiceUrl())
-             .contact(new Contact(swaggerProperties.getContact().getName(), swaggerProperties.getContact().getUrl(), swaggerProperties.getContact().getEmail()))
-             .version(swaggerProperties.getVersion())
-             .build();
-    }
+		ApiSelectorBuilder builder = new Docket(DocumentationType.SWAGGER_2).host(swaggerProperties.getHost())
+				.apiInfo(apiInfo(swaggerProperties)).globalRequestParameters(pars).select()
+				.apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()));
+
+		swaggerProperties.getBasePath().forEach(p -> builder.paths(PathSelectors.ant(p)));
+		swaggerProperties.getExcludePath().forEach(p -> builder.paths(PathSelectors.ant(p).negate()));
+
+		return builder.build().securitySchemes(Collections.singletonList(securitySchema()))
+				.securityContexts(Collections.singletonList(securityContext())).pathMapping("/");
+	}
+
+	/**
+	 * 配置默认的全局鉴权策略的开关，通过正则表达式进行匹配；默认匹配所有URL
+	 * @return
+	 */
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	/**
+	 * 默认的全局鉴权策略
+	 * @return
+	 */
+	private List<SecurityReference> defaultAuth() {
+		ArrayList<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+		swaggerProperties().getAuthorization().getAuthorizationScopeList()
+				.forEach(authorizationScope -> authorizationScopeList.add(
+						new AuthorizationScope(authorizationScope.getScope(), authorizationScope.getDescription())));
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[authorizationScopeList.size()];
+		return Collections
+				.singletonList(SecurityReference.builder().reference(swaggerProperties().getAuthorization().getName())
+						.scopes(authorizationScopeList.toArray(authorizationScopes)).build());
+	}
+
+	private OAuth securitySchema() {
+		ArrayList<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+		swaggerProperties().getAuthorization().getAuthorizationScopeList()
+				.forEach(authorizationScope -> authorizationScopeList.add(
+						new AuthorizationScope(authorizationScope.getScope(), authorizationScope.getDescription())));
+		ArrayList<GrantType> grantTypes = new ArrayList<>();
+		swaggerProperties().getAuthorization().getTokenUrlList()
+				.forEach(tokenUrl -> grantTypes.add(new ResourceOwnerPasswordCredentialsGrant(tokenUrl)));
+		return new OAuth(swaggerProperties().getAuthorization().getName(), authorizationScopeList, grantTypes);
+	}
+
+	private ApiInfo apiInfo(SwaggerProperties swaggerProperties) {
+		return new ApiInfoBuilder().title(swaggerProperties.getTitle()).description(swaggerProperties.getDescription())
+				.license(swaggerProperties.getLicense()).licenseUrl(swaggerProperties.getLicenseUrl())
+				.termsOfServiceUrl(swaggerProperties.getTermsOfServiceUrl())
+				.contact(new Contact(swaggerProperties.getContact().getName(), swaggerProperties.getContact().getUrl(),
+						swaggerProperties.getContact().getEmail()))
+				.version(swaggerProperties.getVersion()).build();
+	}
+
 }
--- SuMu-Cloud/sumu-common/sumu-common-swagger/src/main/java/com/sumu/common/swagger/config/SwaggerProperties.java
+++ SuMu-Cloud/sumu-common/sumu-common-swagger/src/main/java/com/sumu/common/swagger/config/SwaggerProperties.java
@@ -1,346 +1,166 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
 package com.sumu.common.swagger.config;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+/**
+ * SwaggerProperties
+ *
+ * @author lengleng
+ * @date 2018/7/25 14:00
+ */
+@Data
 @ConfigurationProperties("swagger")
-public class SwaggerProperties
-{
-    /**
-     * 是否开启swagger
-     */
-    private Boolean enabled;
+public class SwaggerProperties {
 
-    /**
-     * swagger会解析的包路径
-     **/
-    private String basePackage = "";
+	/**
+	 * 是否开启swagger
+	 */
+	private Boolean enabled;
 
-    /**
-     * swagger会解析的url规则
-     **/
-    private List<String> basePath = new ArrayList<>();
+	/**
+	 * swagger会解析的包路径
+	 **/
+	private String basePackage = "";
 
-    /**
-     * 在basePath基础上需要排除的url规则
-     **/
-    private List<String> excludePath = new ArrayList<>();
+	/**
+	 * swagger会解析的url规则
+	 **/
+	private List<String> basePath = new ArrayList<>();
 
-    /**
-     * 标题
-     **/
-    private String title = "";
+	/**
+	 * 在basePath基础上需要排除的url规则
+	 **/
+	private List<String> excludePath = new ArrayList<>();
 
-    /**
-     * 描述
-     **/
-    private String description = "";
+	/**
+	 * 需要排除的服务
+	 */
+	private List<String> ignoreProviders = new ArrayList<>();
 
-    /**
-     * 版本
-     **/
-    private String version = "";
+	/**
+	 * 标题
+	 **/
+	private String title = "";
 
-    /**
-     * 许可证
-     **/
-    private String license = "";
+	/**
+	 * 描述
+	 **/
+	private String description = "";
 
-    /**
-     * 许可证URL
-     **/
-    private String licenseUrl = "";
+	/**
+	 * 版本
+	 **/
+	private String version = "";
 
-    /**
-     * 服务条款URL
-     **/
-    private String termsOfServiceUrl = "";
+	/**
+	 * 许可证
+	 **/
+	private String license = "";
 
-    /**
-     * host信息
-     **/
-    private String host = "";
+	/**
+	 * 许可证URL
+	 **/
+	private String licenseUrl = "";
 
-    /**
-     * 联系人信息
-     */
-    private Contact contact = new Contact();
+	/**
+	 * 服务条款URL
+	 **/
+	private String termsOfServiceUrl = "";
 
-    /**
-     * 全局统一鉴权配置
-     **/
-    private Authorization authorization = new Authorization();
+	/**
+	 * host信息
+	 **/
+	private String host = "";
 
-    public Boolean getEnabled()
-    {
-        return enabled;
-    }
+	/**
+	 * 联系人信息
+	 */
+	private Contact contact = new Contact();
 
-    public void setEnabled(Boolean enabled)
-    {
-        this.enabled = enabled;
-    }
+	/**
+	 * 全局统一鉴权配置
+	 **/
+	private Authorization authorization = new Authorization();
 
-    public String getBasePackage()
-    {
-        return basePackage;
-    }
+	@Data
+	@NoArgsConstructor
+	public static class Contact {
 
-    public void setBasePackage(String basePackage)
-    {
-        this.basePackage = basePackage;
-    }
+		/**
+		 * 联系人
+		 **/
+		private String name = "";
 
-    public List<String> getBasePath()
-    {
-        return basePath;
-    }
+		/**
+		 * 联系人url
+		 **/
+		private String url = "";
 
-    public void setBasePath(List<String> basePath)
-    {
-        this.basePath = basePath;
-    }
+		/**
+		 * 联系人email
+		 **/
+		private String email = "";
 
-    public List<String> getExcludePath()
-    {
-        return excludePath;
-    }
+	}
 
-    public void setExcludePath(List<String> excludePath)
-    {
-        this.excludePath = excludePath;
-    }
+	@Data
+	@NoArgsConstructor
+	public static class Authorization {
 
-    public String getTitle()
-    {
-        return title;
-    }
+		/**
+		 * 鉴权策略ID，需要和SecurityReferences ID保持一致
+		 */
+		private String name = "";
 
-    public void setTitle(String title)
-    {
-        this.title = title;
-    }
+		/**
+		 * 需要开启鉴权URL的正则
+		 */
+		private String authRegex = "^.*$";
 
-    public String getDescription()
-    {
-        return description;
-    }
+		/**
+		 * 鉴权作用域列表
+		 */
+		private List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
 
-    public void setDescription(String description)
-    {
-        this.description = description;
-    }
+		private List<String> tokenUrlList = new ArrayList<>();
 
-    public String getVersion()
-    {
-        return version;
-    }
+	}
 
-    public void setVersion(String version)
-    {
-        this.version = version;
-    }
+	@Data
+	@NoArgsConstructor
+	public static class AuthorizationScope {
 
-    public String getLicense()
-    {
-        return license;
-    }
+		/**
+		 * 作用域名称
+		 */
+		private String scope = "";
 
-    public void setLicense(String license)
-    {
-        this.license = license;
-    }
+		/**
+		 * 作用域描述
+		 */
+		private String description = "";
 
-    public String getLicenseUrl()
-    {
-        return licenseUrl;
-    }
+	}
 
-    public void setLicenseUrl(String licenseUrl)
-    {
-        this.licenseUrl = licenseUrl;
-    }
-
-    public String getTermsOfServiceUrl()
-    {
-        return termsOfServiceUrl;
-    }
-
-    public void setTermsOfServiceUrl(String termsOfServiceUrl)
-    {
-        this.termsOfServiceUrl = termsOfServiceUrl;
-    }
-
-    public String getHost()
-    {
-        return host;
-    }
-
-    public void setHost(String host)
-    {
-        this.host = host;
-    }
-
-    public Contact getContact()
-    {
-        return contact;
-    }
-
-    public void setContact(Contact contact)
-    {
-        this.contact = contact;
-    }
-
-    public Authorization getAuthorization()
-    {
-        return authorization;
-    }
-
-    public void setAuthorization(Authorization authorization)
-    {
-        this.authorization = authorization;
-    }
-
-    public static class Contact
-    {
-        /**
-         * 联系人
-         **/
-        private String name = "";
-        /**
-         * 联系人url
-         **/
-        private String url = "";
-        /**
-         * 联系人email
-         **/
-        private String email = "";
-
-        public String getName()
-        {
-            return name;
-        }
-
-        public void setName(String name)
-        {
-            this.name = name;
-        }
-
-        public String getUrl()
-        {
-            return url;
-        }
-
-        public void setUrl(String url)
-        {
-            this.url = url;
-        }
-
-        public String getEmail()
-        {
-            return email;
-        }
-
-        public void setEmail(String email)
-        {
-            this.email = email;
-        }
-    }
-
-    public static class Authorization
-    {
-        /**
-         * 鉴权策略ID，需要和SecurityReferences ID保持一致
-         */
-        private String name = "";
-
-        /**
-         * 需要开启鉴权URL的正则
-         */
-        private String authRegex = "^.*$";
-
-        /**
-         * 鉴权作用域列表
-         */
-        private List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
-
-        private List<String> tokenUrlList = new ArrayList<>();
-
-        public String getName()
-        {
-            return name;
-        }
-
-        public void setName(String name)
-        {
-            this.name = name;
-        }
-
-        public String getAuthRegex()
-        {
-            return authRegex;
-        }
-
-        public void setAuthRegex(String authRegex)
-        {
-            this.authRegex = authRegex;
-        }
-
-        public List<AuthorizationScope> getAuthorizationScopeList()
-        {
-            return authorizationScopeList;
-        }
-
-        public void setAuthorizationScopeList(List<AuthorizationScope> authorizationScopeList)
-        {
-            this.authorizationScopeList = authorizationScopeList;
-        }
-
-        public List<String> getTokenUrlList()
-        {
-            return tokenUrlList;
-        }
-
-        public void setTokenUrlList(List<String> tokenUrlList)
-        {
-            this.tokenUrlList = tokenUrlList;
-        }
-    }
-
-    public static class AuthorizationScope
-    {
-        /**
-         * 作用域名称
-         */
-        private String scope = "";
-
-        /**
-         * 作用域描述
-         */
-        private String description = "";
-
-        public String getScope()
-        {
-            return scope;
-        }
-
-        public void setScope(String scope)
-        {
-            this.scope = scope;
-        }
-
-        public String getDescription()
-        {
-            return description;
-        }
-
-        public void setDescription(String description)
-        {
-            this.description = description;
-        }
-    }
 }
