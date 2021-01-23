c2b2ff68409896c3c58f3c9bd740a1eb2a6b6e07
fengjian <fengj@whalekun.com>
2021-01-23 23:14:28 +0800

    初始化log

---
+++ SuMu-Cloud/sumu-common/sumu-common-log/src/main/java/com/sumu/common/log/aspect/SysLogAspect.java
@@ -0,0 +1,54 @@
+package com.sumu.common.log.aspect;
+
+import com.sumu.admin.api.entity.SysLog;
+import com.sumu.common.core.util.SpringContextHolder;
+import com.sumu.common.log.event.SysLogEvent;
+import com.sumu.common.log.util.LogTypeEnum;
+import com.sumu.common.log.util.SysLogUtils;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * 操作日志使用spring event异步入库
+ *
+ * @author L.cm
+ */
+@Aspect
+@Slf4j
+public class SysLogAspect {
+
+	@Around("@annotation(sysLog)")
+	@SneakyThrows
+	public Object around(ProceedingJoinPoint point, com.sumu.common.log.annotation.SysLog sysLog) {
+		String strClassName = point.getTarget().getClass().getName();
+		String strMethodName = point.getSignature().getName();
+		log.debug("[类名]:{},[方法]:{}", strClassName, strMethodName);
+
+		SysLog logVo = SysLogUtils.getSysLog();
+		logVo.setTitle(sysLog.value());
+
+		// 发送异步日志事件
+		Long startTime = System.currentTimeMillis();
+		Object obj;
+
+		try {
+			obj = point.proceed();
+		}
+		catch (Exception e) {
+			logVo.setType(LogTypeEnum.ERROR.getType());
+			logVo.setException(e.getMessage());
+			throw e;
+		}
+		finally {
+			Long endTime = System.currentTimeMillis();
+			logVo.setTime(endTime - startTime);
+			SpringContextHolder.publishEvent(new SysLogEvent(logVo));
+		}
+
+		return obj;
+	}
+
+}
---
+++ SuMu-Cloud/sumu-common/sumu-common-log/src/main/java/com/sumu/common/log/util/SysLogUtils.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020 pig4cloud Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sumu.common.log.util;
+
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.extra.servlet.ServletUtil;
+import cn.hutool.http.HttpUtil;
+import com.sumu.admin.api.entity.SysLog;
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+/**
+ * 系统日志工具类
+ *
+ * @author L.cm
+ */
+@UtilityClass
+public class SysLogUtils {
+
+	public SysLog getSysLog() {
+		HttpServletRequest request = ((ServletRequestAttributes) Objects
+				.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+		SysLog sysLog = new SysLog();
+		sysLog.setCreateBy(Objects.requireNonNull(getUsername()));
+		sysLog.setType(LogTypeEnum.NORMAL.getType());
+		sysLog.setRemoteAddr(ServletUtil.getClientIP(request));
+		sysLog.setRequestUri(URLUtil.getPath(request.getRequestURI()));
+		sysLog.setMethod(request.getMethod());
+		sysLog.setUserAgent(request.getHeader("user-agent"));
+		sysLog.setParams(HttpUtil.toParams(request.getParameterMap()));
+		sysLog.setServiceId(getClientId());
+		return sysLog;
+	}
+
+	/**
+	 * 获取客户端
+	 * @return clientId
+	 */
+	private String getClientId() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication instanceof OAuth2Authentication) {
+			OAuth2Authentication auth2Authentication = (OAuth2Authentication) authentication;
+			return auth2Authentication.getOAuth2Request().getClientId();
+		}
+		return null;
+	}
+
+	/**
+	 * 获取用户名称
+	 * @return username
+	 */
+	private String getUsername() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null) {
+			return null;
+		}
+		return authentication.getName();
+	}
+
+}
